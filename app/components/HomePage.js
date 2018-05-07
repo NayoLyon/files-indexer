d1708076768bd69ae2e7299f34c593f105fe80fe
NayoLyon <lionel.a.martin@gmail.com>
2018-05-07 23:04:11 +0200

    Clean log

--- app/components/HomePage.js
+++ app/components/HomePage.js
@@ -25,7 +25,6 @@ class HomePage extends Component<Props> {
   props: Props;
 
   selectFolder(isMaster: boolean) {
-    console.log(`selectFolder(${isMaster})`, this);
     dialog.showOpenDialog({
       properties: ['openDirectory']
     }, this.setFolder(isMaster));
