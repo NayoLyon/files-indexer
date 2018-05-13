d28f19e9dbf4f24cff5292c10d8d8abbac0cb2d5
NayoLyon <lionel.a.martin@gmail.com>
2018-05-13 18:13:42 +0200

    Refactor: remove scan processFile to scanActions
    It will be used by the ScanResultPage, when updating DbFile's properties...

--- app/components/ScanPage.js
+++ app/components/ScanPage.js
@@ -5,7 +5,6 @@ import { connect } from 'react-redux';
 
 import * as ScanActions from '../modules/scan/scanAction';
 import { doScan, FileProps, FilePropsDb } from '../api/filesystem';
-import { findDb } from '../api/database';
 
 import Scan from './Scan';
 
@@ -13,58 +12,17 @@ type Props = {
   scanProgress: (string, number) => void,
   startScan: () => void,
   endScan: () => void,
-  scanExistsAdd: FileProps => void,
-  scanNewAdd: FileProps => void,
-  scanModifiedAdd: (FileProps, Map<string, Array<string | number | Date>>, FilePropsDb) => void,
-  scanDuplicateAdd: (FileProps, Arrays<FilePropsDb>) => void,
-  scanRefAdd: (file: FileProps, dbFile: FilePropsDb, scanType: string) => void,
-  masterFolder: string,
+  scanProcessFile: (fileProps: FileProps, oldDbFile: FilePropsDb | undefined) => void,
   toScanFolder: string
 };
 
 class ScanPage extends Component<Props> {
   props: Props;
-  constructor(props) {
-    super(props);
-    this.processFile = this.processFile.bind(this);
-  }
-
-  async processFile(fileProps: FileProps) {
-    let occurences = await findDb(this.props.masterFolder, { _id: fileProps.id });
-    if (occurences.length === 0) {
-      // File not found in db... Search for files with similar properties
-      occurences = await findDb(this.props.masterFolder, {
-        name: fileProps.name
-      });
-      if (occurences.length === 0) {
-        this.props.scanNewAdd(fileProps);
-      } else {
-        this.props.scanDuplicateAdd(fileProps, occurences);
-        occurences.forEach(elt => {
-          this.props.scanRefAdd(fileProps, elt, ScanActions.CONST_SCAN_TYPE_DUPLICATE);
-        });
-      }
-    } else {
-      if (occurences.length > 1) {
-        console.error(occurences);
-        throw Error(`Multiple occurences from hash ${fileProps.id}!!`);
-      }
-      const inDb = occurences[0];
-      const compared: Map<string, Array<string | number | Date>> = fileProps.compareSameHash(inDb);
-      if (compared.size > 0) {
-        this.props.scanModifiedAdd(fileProps, compared, inDb);
-        this.props.scanRefAdd(fileProps, inDb, ScanActions.CONST_SCAN_TYPE_MODIFIED);
-      } else {
-        this.props.scanExistsAdd(fileProps);
-        this.props.scanRefAdd(fileProps, inDb, ScanActions.CONST_SCAN_TYPE_EXISTS);
-      }
-    }
-  }
 
   async scan() {
     this.props.startScan();
 
-    await doScan(this.props.toScanFolder, this.processFile, this.props.scanProgress);
+    await doScan(this.props.toScanFolder, this.props.scanProcessFile, this.props.scanProgress);
 
     this.props.endScan();
   }
@@ -76,7 +34,6 @@ class ScanPage extends Component<Props> {
 
 function mapStateToProps(state) {
   return {
-    masterFolder: state.folders.masterPath,
     toScanFolder: state.folders.toScanPath
   };
 }
--- app/modules/scan/scanAction.js
+++ app/modules/scan/scanAction.js
@@ -1,5 +1,7 @@
 // @flow
 import { FileProps, FilePropsDb } from '../../api/filesystem';
+import { findDb } from '../../api/database';
+import { Action } from '../actionType';
 
 export const SCAN_START = 'SCAN_START';
 export const SCAN_END = 'SCAN_END';
@@ -131,5 +133,46 @@ export function scanRefUpdate(
     oldDbFile,
     newDbFile,
     scanType
+  };
+}
+
+export function scanProcessFile(fileProps: FileProps, oldDbFile: FilePropsDb | undefined) {
+  return async (dispatch: (action: Action) => void, getState) => {
+    const { masterPath } = getState().folders;
+
+    let occurences = await findDb(masterPath, { _id: fileProps.id });
+    if (occurences.length === 0) {
+      // File not found in db... Search for files with similar properties
+      occurences = await findDb(masterPath, {
+        name: fileProps.name
+      });
+      if (occurences.length === 0) {
+        dispatch(scanNewAdd(fileProps));
+      } else {
+        dispatch(scanDuplicateAdd(fileProps, occurences));
+        occurences.forEach(elt => {
+          dispatch(scanRefUpdate(
+            fileProps,
+            oldDbFile,
+            elt,
+            CONST_SCAN_TYPE_DUPLICATE
+          ));
+        });
+      }
+    } else {
+      if (occurences.length > 1) {
+        console.error(occurences);
+        throw Error(`Multiple occurences from hash ${fileProps.id}!!`);
+      }
+      const inDb = occurences[0];
+      const compared: Map<string, Array<string | number | Date>> = fileProps.compareSameHash(inDb);
+      if (compared.size > 0) {
+        dispatch(scanModifiedAdd(fileProps, compared, inDb));
+        dispatch(scanRefUpdate(fileProps, oldDbFile, inDb, CONST_SCAN_TYPE_MODIFIED));
+      } else {
+        dispatch(scanExistsAdd(fileProps));
+        dispatch(scanRefUpdate(fileProps, oldDbFile, inDb, CONST_SCAN_TYPE_EXISTS));
+      }
+    }
   };
 }
