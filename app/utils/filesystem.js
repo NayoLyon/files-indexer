a243a7b309185c55b146326f912df806afbe94af
NayoLyon <lionel.a.martin@gmail.com>
2018-05-23 00:15:43 +0200

    Add function to openExplorer for a folder

--- app/utils/filesystem.js
+++ app/utils/filesystem.js
@@ -3,6 +3,9 @@ import { shell } from 'electron';
 export function openExplorerOn(folder) {
   shell.showItemInFolder(folder);
 }
+export function openExplorerFor(folder) {
+  shell.openItem(folder);
+}
 
 export function deleteFile(filePath) {
   shell.moveItemToTrash(filePath);
