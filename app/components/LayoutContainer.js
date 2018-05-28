c0bb85468513d1529d8e2e560caec9fe45f918fc
NayoLyon <lionel.a.martin@gmail.com>
2018-05-28 23:24:48 +0200

    Fix redirect to Index on reload

--- app/components/LayoutContainer.js
+++ app/components/LayoutContainer.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux';
 
 import HomeContainer from './home/HomeContainer';
 import IndexationContainer from './indexation/IndexationContainer';
@@ -9,15 +11,18 @@ import ScanContainer from './scan/ScanContainer';
 import AnalyseContainer from './analyseDb/AnalyseContainer';
 
 type Props = {
+  push: string => void,
   dbLoaded: boolean
 };
 
 class LayoutContainer extends Component<Props> {
   props: Props;
-  render() {
+  componentDidMount() {
     if (!this.props.dbLoaded) {
-      return <IndexationContainer />;
+      this.props.push('/index');
     }
+  }
+  render() {
     return (
       <Switch>
         <Route exact path="/scan" component={ScanContainer} />
@@ -34,5 +39,8 @@ function mapStateToProps(state) {
     dbLoaded: state.indexationState.dbLoaded
   };
 }
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ push }, dispatch);
+}
 
-export default connect(mapStateToProps)(LayoutContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(LayoutContainer);
