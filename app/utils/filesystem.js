24e825128ebf9a77d00f1d4c2c092791c6c68cb6
NayoLyon <lionel.a.martin@gmail.com>
2018-05-30 00:15:00 +0200

    Add: try to remove empty directories (or directories with ignored files like Thumbs.db
    Also add a commented function that checks that a full path is a child of another. Useless for now as we have the root path and a relative path

--- app/utils/filesystem.js
+++ app/utils/filesystem.js
@@ -1,4 +1,5 @@
 import { shell } from 'electron';
+import fs from 'fs';
 import path from 'path';
 
 export function openExplorerOn(folder) {
@@ -11,7 +12,31 @@ export function openExplorerFor(folder) {
 export function deleteFile(rootFolder, relPath) {
   const filePath = path.resolve(rootFolder, relPath);
   shell.moveItemToTrash(filePath);
+  if (rootFolder) {
+    tryDeleteFolder(rootFolder, path.dirname(relPath));
+  }
 }
+
+function tryDeleteFolder(rootFolder, relFolder) {
+  if (relFolder === '.') {
+    return;
+  }
+  const folder = path.resolve(rootFolder, relFolder);
+  const files = fs.readdirSync(folder);
+  const eligibleFiles = files.filter(file => isEligibleFile(file));
+  if (eligibleFiles.length === 0) {
+    shell.moveItemToTrash(folder);
+    tryDeleteFolder(rootFolder, path.dirname(relFolder));
+  }
+}
+
+// function isChildOf(folder, rootFolder) {
+//   if (folder === rootFolder) {
+//     return false;
+//   }
+//   const relative = path.relative(rootFolder, folder);
+//   return !!relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+// }
 
 export function isEligibleFile(file) {
   return !['.index.db', 'Thumbs.db'].includes(file);
