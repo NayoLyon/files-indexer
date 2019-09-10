0289849ef6e16e5a3948145e37028a7b6a0a2002
NayoLyon <lionel.a.martin@gmail.com>
2019-09-10 22:02:25 +0200

    Refactor to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 // import Store from "./ElectronStore";
 
@@ -12,25 +12,26 @@ import AppLayout from "./components/layouts/AppLayout";
 // 	}
 // } = window.require("electron");
 
-class App extends Component {
-	componentDidMount() {
-		window.addEventListener("dragenter", App.preventDrop, false);
-		window.addEventListener("dragover", App.preventDrop, false);
-		window.addEventListener("drop", App.preventDrop, false);
+const preventDrop = e => e.preventDefault();
+const App = () => {
+	useEffect(() => {
+		window.addEventListener("dragenter", preventDrop, false);
+		window.addEventListener("dragover", preventDrop, false);
+		window.addEventListener("drop", preventDrop, false);
+		loadConfig();
 
-		this.props.dispatch(loadConfig());
-	}
-	componentWillUnmount() {
-		window.removeEventListener("dragenter", App.preventDrop, false);
-		window.removeEventListener("dragover", App.preventDrop, false);
-		window.removeEventListener("drop", App.preventDrop, false);
-	}
-	static preventDrop(e) {
-		e.preventDefault();
-	}
-	render() {
-		return <AppLayout />;
-	}
-}
+		return () => {
+			window.removeEventListener("dragenter", preventDrop, false);
+			window.removeEventListener("dragover", preventDrop, false);
+			window.removeEventListener("drop", preventDrop, false);
+		};
+	}, []);
+	return <AppLayout />;
+};
 
-export default connect()(App);
+const mapDispatchToProps = dispatch => ({ loadConfig: dispatch(loadConfig()) });
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(App);
