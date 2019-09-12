d0d60ae0a43313d3a9ab9d8a77022c8a78fc59c4
NayoLyon <lionel.a.martin@gmail.com>
2019-09-12 20:18:40 +0200

    Refactor: convert component to functional components

--- src/components/indexation/IndexationAnomaliesView.js
+++ src/components/indexation/IndexationAnomaliesView.js
@@ -1,71 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 import { Table } from "semantic-ui-react";
 
 import { printValue } from "../../utils/format";
 
-class IndexationAnomaliesView extends Component {
-	static renderAnomaly(dupFile, relpath, listItems) {
-		const { dbFile, file, diff } = dupFile;
-		let isFirst = true;
-		diff.forEach(prop => {
-			let mainCell = null;
-			if (isFirst) {
-				mainCell = (
-					<Table.Cell key="relpath" textAlign="center" rowSpan={diff.size}>
-						{relpath}
-					</Table.Cell>
-				);
-			}
-			let rowContent;
-			if (prop === "new") {
-				rowContent = [
-					<Table.Cell key="prop" textAlign="center">
-						{prop}
-					</Table.Cell>,
-					<Table.Cell key="filevalue" textAlign="center" />,
-					<Table.Cell key="dbvalue" />
-				];
-			} else {
-				rowContent = [
-					<Table.Cell key="prop" textAlign="center">
-						{prop}
-					</Table.Cell>,
-					<Table.Cell key="filevalue" textAlign="center">
-						{printValue(file, prop)}
-					</Table.Cell>,
-					<Table.Cell key="dbvalue" textAlign="center">
-						{printValue(dbFile, prop)}
-					</Table.Cell>
-				];
-			}
-			/* eslint-disable react/no-array-index-key */
-			listItems.push(
-				<Table.Row key={`${relpath}_${prop}`}>
-					{mainCell}
-					{rowContent}
-				</Table.Row>
-			);
-			/* eslint-enable react/no-array-index-key */
-			isFirst = false;
-		});
-	}
+const IndexationAnomaliesDuplicate = ({ dupFile: { dbFile, file, diff }, relpath }) =>
+	Array.from(diff).map((prop, index) => (
+		<Table.Row key={`${relpath}_${prop}`}>
+			{index === 0 && (
+				<Table.Cell textAlign="center" rowSpan={diff.size}>
+					{relpath}
+				</Table.Cell>
+			)}
+			<Table.Cell textAlign="center">{prop}</Table.Cell>
+			<Table.Cell textAlign="center">
+				{prop === "new" ? null : printValue(file, prop)}
+			</Table.Cell>
+			<Table.Cell textAlign="center">
+				{prop === "new" ? null : printValue(dbFile, prop)}
+			</Table.Cell>
+		</Table.Row>
+	));
 
-	render() {
-		if (this.props.duplicates.size === 0) {
-			return null;
-		}
-
-		const listItems = [];
-		this.props.duplicates.forEach((dupFile, relpath) => {
-			IndexationAnomaliesView.renderAnomaly(dupFile, relpath, listItems);
-		});
-
-		return (
-			<Table>
-				<Table.Body>{listItems}</Table.Body>
-			</Table>
+const IndexationAnomaliesView = ({ duplicates }) => {
+	const duplicatesRows = [];
+	duplicates.forEach((dupFile, relpath) => {
+		duplicatesRows.push(
+			<IndexationAnomaliesDuplicate key={relpath} dupFile={dupFile} relpath={relpath} />
 		);
-	}
-}
+	});
+
+	return duplicates.size === 0 ? null : (
+		<Table>
+			<Table.Body>{duplicatesRows}</Table.Body>
+		</Table>
+	);
+};
 
 export default IndexationAnomaliesView;
